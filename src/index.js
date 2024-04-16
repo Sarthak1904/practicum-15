ef48efe996acf03b2ad2519ba81749d5083fe45d
Sarthak1904 <csarthak051@gmail.com>
2024-04-16 18:07:58 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,52 @@
-require('dotenv').config()
 const express = require('express');
 const app = express();
+const port = 3000;
 const cors = require('cors');
-const path = require('path');
 
-const publicDirectoryPath = path.join(__dirname, '../');
-
-if (!process.env.PORT) {
-  throw new Error('Please specify the port number for the HTTP server with the environment variable PORT.');
-  }
-  const port = process.env.PORT;
-  app.get('/', (req, res) => {
-  res.send('Arithmetic service - last updated 3/4/2022');
-  });
-  
-
-
-app.use(cors());
-
-app.use(express.static(publicDirectoryPath));
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(publicDirectoryPath, 'index.html'));
+    res.send('Arithmatic Service - Hello World!');
 });
 
 app.get('/add/:n/:m', (req, res) => {
-  const result = Number(req.params.n) + Number(req.params.m);
-  res.json({ sum: result });
+    const num1 = parseInt(req.params.n);
+    const num2 = parseInt(req.params.m);
+    const sum = num1 + num2;
+    res.json(sum);
 });
 
-// Prime number check endpoint
-app.get('/isPrime/:number', (req, res) => {
-  const number = parseInt(req.params.number, 10);
-  const isPrimeResult = isPrime(number);
-  res.json({ 
-    number: number,
-    isPrime: isPrimeResult,
-    message: isPrimeResult ? `${number} is a prime number.` : `${number} is not a prime number.`
-  });
+app.get('/isprime/:n', (req, res) => {
+    result = ''
+    const num = parseInt(req.params.n);
+    const isPrime = checkPrime(num);
+    if(isPrime) {
+        result = 'Prime';
+    }
+    else{
+        result = 'Not Prime'
+    }
+    res.json(result);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-function isPrime(number) {
-  if (number < 2) return false;
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) return false;
-  }
-  return true;
+function checkPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    if (num <= 3) {
+        return true;
+    }
+    if (num % 2 === 0 || num % 3 === 0) {
+        return false;
+    }
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) {
+            return false;
+        }
+    }
+    return true;
 }
+
+//listening port
+app.listen(port);
